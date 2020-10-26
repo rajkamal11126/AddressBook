ee080883335956a9a0a0786d1144aa0054163054
Raj <rajkamal11126@gmail.com>
2020-10-26 11:07:37 +0530

    Added : method to edit details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
+	ArrayList<PersonInfo> persons;
+
+	public AddressBook() {
+		persons = new ArrayList<PersonInfo>();
+	}
+	//method to add details
 	public void addPerson() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your name");
@@ -17,6 +24,20 @@ public class AddressBook {
 		String zip = scan.nextLine();
 		System.out.println("Enter your email");
 		String email = scan.nextLine();
+		
+		// construct new person object
+		PersonInfo p = new PersonInfo(name, add, pNum, city, state, zip, email);
+		persons.add(p);
+	}
+
+	// method to edit the person details
+	public void editPerson(String n) {
+		for (int i = 0; i < persons.size(); i++) {
+			PersonInfo p = (PersonInfo) persons.get(i);
+			if (n.equals(p.getName())) {
+				p.print();
+			}
+		}
 	}
 
 }
--- src/TestBook.java
+++ src/TestBook.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
 
 public class TestBook {
 	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
 		AddressBook ab = new AddressBook();
-		System.out.println("enter person detail");
-		ab.addPerson();
+		String input, s;
+		int cs;
+		while (true) {
+			System.out.println("enter 1 for person detail");
+			System.out.println("enter 2 for edit");
+			System.out.println("enter 3 for exit");
+			input = scan.nextLine();
+			cs = Integer.parseInt(input);
+			switch (cs) {
+			case 1:
+				ab.addPerson();
+				break;
+			case 2:
+				System.out.println("Enter name to edit");
+				s = scan.next();
+				ab.editPerson(s);
+				break;
+			case 3:
+				System.exit(0);
+			}
+		}
 	}
 
 }
